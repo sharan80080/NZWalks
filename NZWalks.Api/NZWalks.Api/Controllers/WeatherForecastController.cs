531330554058bc33909ebfcae93408b5182c6678
sharan <sharankmr46@gmail.com>
2023-03-04 14:14:29 +0530

    Update WeatherForecastController.cs
    Added Items in string object

--- NZWalks.Api/NZWalks.Api/Controllers/WeatherForecastController.cs
+++ NZWalks.Api/NZWalks.Api/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace NZWalks.Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Windy","Humid","stromy"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
